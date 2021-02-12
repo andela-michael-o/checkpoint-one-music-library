507326d9ac78c2ac48282147eae75af9ab544ed0
michaelogala <mykel41@gmail.com>
2021-02-12 13:55:11 +0100

    song basics

--- lib/song.rb
+++ lib/song.rb
@@ -1,39 +1,28 @@
-class Song < MusicBase
-  attr_reader :artist, :genre
+class Song
+
   attr_accessor :name
+
   @@all = []
 
-  def initialize(name, artist = nil, genre = nil)
+  def initialize(name)
     @name = name
-    self.artist = artist if artist.is_a? Artist
-    self.genre = genre if genre.is_a? Genre
-    self.artist.add_genre(genre) unless genre.nil?
-    genre.artists = artist unless genre.nil? || artist.nil?
   end
 
-  def artist=(artist)
-    artist.add_song(self) unless artist.songs.include?(self)
-    @artist ||= artist
+  def save
+    self.class.class_variable_get(:@@all) << self
   end
 
-  def set_artist(artist)
-    @artist = artist
-  end
+  class << self
+    def create(*args)
+      new(*args).tap { |s| s.save }
+    end
 
-  def genre=(genre)
-    genre.songs << self unless genre.songs.include?(self)
-    @genre = genre
-  end
+    def all
+      class_variable_get(:@@all)
+    end
 
-  def self.new_from_filename(filename)
-    split_names = filename.gsub('.mp3', '').split(' - ')
-    song = Song.find_or_create_by_name(split_names[1])
-    song.artist = Artist.find_or_create_by_name(split_names[0])
-    song.genre = Genre.find_or_create_by_name(split_names[2])
-    song
-  end
-
-  def self.create_from_filename(file_name)
-    new_from_filename(file_name).save
+    def destroy_all
+      class_variable_set(:@@all, [])
+    end
   end
 end
